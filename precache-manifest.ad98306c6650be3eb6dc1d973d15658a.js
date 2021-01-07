3741a3a8315c9507e1d8bcd0221032b999022652
Lemon <516330797@qq.com>
2021-01-07 10:46:13 +0800

    deploy

---
+++ precache-manifest.ad98306c6650be3eb6dc1d973d15658a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "607891f048c4fa2179b4",
+    "url": "/morney-vue-website/css/app.986440fb.css"
+  },
+  {
+    "revision": "d30ba1cc5d688ce5b4ac9b39fdeb4d67",
+    "url": "/morney-vue-website/index.html"
+  },
+  {
+    "revision": "607891f048c4fa2179b4",
+    "url": "/morney-vue-website/js/app.214f47e1.js"
+  },
+  {
+    "revision": "07dce5b39931f45e3d0b",
+    "url": "/morney-vue-website/js/chunk-vendors.9f8cd3a0.js"
+  },
+  {
+    "revision": "4b700bda4a3f05b842705dfbb2508bee",
+    "url": "/morney-vue-website/manifest.json"
+  },
+  {
+    "revision": "88afdd898f62a2e599e6dada73d3b497",
+    "url": "/morney-vue-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-vue-website/robots.txt"
+  }
+]);
